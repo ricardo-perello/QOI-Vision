012343dbedb19f989c779403d4ceb6b23156b921
rperellomas <ricardo.perellomas@epfl.ch>
2022-11-01 18:34:17 +0100

    first unfinished function

--- src/cs107/ArrayUtils.java
+++ src/cs107/ArrayUtils.java
@@ -26,7 +26,20 @@ public final class ArrayUtils {
      * @throws AssertionError if one of the parameters is null
      */
     public static boolean equals(byte[] a1, byte[] a2){
-        return Helper.fail("Not Implemented");
+        if (a1.length != a2.length){
+            return false;
+        }
+        if (a1.length == 0){
+            return true;
+        }
+        for (int i = 0; i < a1.length; i++){
+
+            if (a1[i]!=a2[i]){
+                return false;
+            }
+
+        }
+        return true;
     }
 
     /**
