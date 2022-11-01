8b195ba3ed4d71b9db2c3aa1367a775daa8d5485
rperellomas <ricardo.perellomas@epfl.ch>
2022-11-01 19:13:54 +0100

    lets gooooo 4.1

--- src/cs107/ArrayUtils.java
+++ src/cs107/ArrayUtils.java
@@ -2,9 +2,10 @@ package cs107;
 
 /**
  * Utility class to manipulate arrays.
- * @apiNote First Task of the 2022 Mini Project
+ *
  * @author Hamza REMMAL (hamza.remmal@epfl.ch)
  * @version 1.3
+ * @apiNote First Task of the 2022 Mini Project
  * @since 1.0
  */
 public final class ArrayUtils {
@@ -12,7 +13,8 @@ public final class ArrayUtils {
     /**
      * DO NOT CHANGE THIS, MORE ON THAT IN WEEK 7.
      */
-    private ArrayUtils(){}
+    private ArrayUtils() {
+    }
 
     // ==================================================================================
     // =========================== ARRAY EQUALITY METHODS ===============================
@@ -20,38 +22,77 @@ public final class ArrayUtils {
 
     /**
      * Check if the content of both arrays is the same
+     *
      * @param a1 (byte[]) - First array
      * @param a2 (byte[]) - Second array
      * @return (boolean) - true if both arrays have the same content (or both null), false otherwise
      * @throws AssertionError if one of the parameters is null
      */
-    public static boolean equals(byte[] a1, byte[] a2){
+    public static boolean equals(byte[] a1, byte[] a2) {
 
-        if (((a1.length == 0)&&(a2.length!=0))||((a2.length == 0)&&(a1.length!= 0))){
+        if (((a1.length == 0) && (a2.length != 0)) || ((a2.length == 0) && (a1.length != 0))) {
             assert (true);
         }
-        if (a1.length != a2.length){
+        if (a1.length != a2.length) {
             return false;
         }
-        for (int i = 0; i < a1.length; i++){
 
-            if (a1[i]!=a2[i]){
+        if (a1.length == 0) {
+            return true;
+        }
+
+        for (int i = 0; i < a1.length; i++) {
+
+            if (a1[i] != a2[i]) {
                 return false;
             }
-
         }
         return true;
     }
 
     /**
      * Check if the content of both arrays is the same
+     *
      * @param a1 (byte[][]) - First array
      * @param a2 (byte[][]) - Second array
      * @return (boolean) - true if both arrays have the same content (or both null), false otherwise
      * @throws AssertionError if one of the parameters is null
      */
-    public static boolean equals(byte[][] a1, byte[][] a2){
-        return Helper.fail("Not Implemented");
+    public static boolean equals(byte[][] a1, byte[][] a2) {
+        if (((a1.length == 0) && (a2.length != 0)) || ((a2.length == 0) && (a1.length != 0))) {
+            assert (true);
+        }
+
+        if (a1.length != a2.length) {
+            return false;
+        }
+
+        if (a1.length == 0) {
+            return true;
+        }
+
+
+        for (int i = 0; i < a1.length; i++) {
+
+            if (((a1[i].length == 0) && (a2[i].length != 0)) || ((a2[i].length == 0) && (a1[i].length != 0))) {
+                assert (true);
+            }
+
+            if (a1[i].length != a2[i].length) {
+                return false;
+            }
+
+            if (a1[i].length == 0) {
+                return true;
+            }
+
+            for (int j = 0; j < a1[i].length; j++) {
+                if (a1[i][j] != a2[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     // ==================================================================================
@@ -60,10 +101,11 @@ public final class ArrayUtils {
 
     /**
      * Wrap the given value in an array
+     *
      * @param value (byte) - value to wrap
      * @return (byte[]) - array with one element (value)
      */
-    public static byte[] wrap(byte value){
+    public static byte[] wrap(byte value) {
         return Helper.fail("Not Implemented");
     }
 
@@ -75,21 +117,23 @@ public final class ArrayUtils {
      * Create an Integer using the given array. The input needs to be considered
      * as "Big Endian"
      * (See handout for the definition of "Big Endian")
+     *
      * @param bytes (byte[]) - Array of 4 bytes
      * @return (int) - Integer representation of the array
      * @throws AssertionError if the input is null or the input's length is different from 4
      */
-    public static int toInt(byte[] bytes){
+    public static int toInt(byte[] bytes) {
         return Helper.fail("Not Implemented");
     }
 
     /**
      * Separate the Integer (word) to 4 bytes. The Memory layout of this integer is "Big Endian"
      * (See handout for the definition of "Big Endian")
+     *
      * @param value (int) - The integer
      * @return (byte[]) - Big Endian representation of the integer
      */
-    public static byte[] fromInt(int value){
+    public static byte[] fromInt(int value) {
         return Helper.fail("Not Implemented");
     }
 
@@ -99,22 +143,24 @@ public final class ArrayUtils {
 
     /**
      * Concatenate a given sequence of bytes and stores them in an array
+     *
      * @param bytes (byte ...) - Sequence of bytes to store in the array
      * @return (byte[]) - Array representation of the sequence
      * @throws AssertionError if the input is null
      */
-    public static byte[] concat(byte ... bytes){
+    public static byte[] concat(byte... bytes) {
         return Helper.fail("Not Implemented");
     }
 
     /**
      * Concatenate a given sequence of arrays into one array
+     *
      * @param tabs (byte[] ...) - Sequence of arrays
      * @return (byte[]) - Array representation of the sequence
      * @throws AssertionError if the input is null
-     * or one of the inner arrays of input is null.
+     *                        or one of the inner arrays of input is null.
      */
-    public static byte[] concat(byte[] ... tabs){
+    public static byte[] concat(byte[]... tabs) {
         return Helper.fail("Not Implemented");
     }
 
@@ -124,28 +170,30 @@ public final class ArrayUtils {
 
     /**
      * Extract an array from another array
-     * @param input (byte[]) - Array to extract from
-     * @param start (int) - Index in the input array to start the extract from
+     *
+     * @param input  (byte[]) - Array to extract from
+     * @param start  (int) - Index in the input array to start the extract from
      * @param length (int) - The number of bytes to extract
      * @return (byte[]) - The extracted array
      * @throws AssertionError if the input is null or start and length are invalid.
-     * start + length should also be smaller than the input's length
+     *                        start + length should also be smaller than the input's length
      */
-    public static byte[] extract(byte[] input, int start, int length){
+    public static byte[] extract(byte[] input, int start, int length) {
         return Helper.fail("Not Implemented");
     }
 
     /**
      * Create a partition of the input array.
      * (See handout for more information on how this method works)
+     *
      * @param input (byte[]) - The original array
      * @param sizes (int ...) - Sizes of the partitions
      * @return (byte[][]) - Array of input's partitions.
      * The order of the partition is the same as the order in sizes
      * @throws AssertionError if one of the parameters is null
-     * or the sum of the elements in sizes is different from the input's length
+     *                        or the sum of the elements in sizes is different from the input's length
      */
-    public static byte[][] partition(byte[] input, int ... sizes) {
+    public static byte[][] partition(byte[] input, int... sizes) {
         return Helper.fail("Not Implemented");
     }
 
@@ -159,12 +207,13 @@ public final class ArrayUtils {
      * a 2-dim byte array where the first dimension is the pixel
      * and the second dimension is the channel.
      * See handouts for more information on the format.
+     *
      * @param input (int[][]) - image data
      * @return (byte [][]) - formatted image data
      * @throws AssertionError if the input is null
-     * or one of the inner arrays of input is null
+     *                        or one of the inner arrays of input is null
      */
-    public static byte[][] imageToChannels(int[][] input){
+    public static byte[][] imageToChannels(int[][] input) {
         return Helper.fail("Not Implemented");
     }
 
@@ -172,17 +221,18 @@ public final class ArrayUtils {
      * Format a 2-dim byte array where the first dimension is the pixel
      * and the second is the channel to a 2-dim int array where the first
      * dimension is the height and the second is the width
-     * @param input (byte[][]) : linear representation of the image
+     *
+     * @param input  (byte[][]) : linear representation of the image
      * @param height (int) - Height of the resulting image
-     * @param width (int) - Width of the resulting image
+     * @param width  (int) - Width of the resulting image
      * @return (int[][]) - the image data
      * @throws AssertionError if the input is null
-     * or one of the inner arrays of input is null
-     * or input's length differs from width * height
-     * or height is invalid
-     * or width is invalid
+     *                        or one of the inner arrays of input is null
+     *                        or input's length differs from width * height
+     *                        or height is invalid
+     *                        or width is invalid
      */
-    public static int[][] channelsToImage(byte[][] input, int height, int width){
+    public static int[][] channelsToImage(byte[][] input, int height, int width) {
         return Helper.fail("Not Implemented");
     }
 
