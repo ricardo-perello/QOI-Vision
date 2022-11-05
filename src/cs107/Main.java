d32f21a69eec70aa2b84c8c481b018e79138d06d
rperellomas <ricardo.perellomas@epfl.ch>
2022-11-05 17:44:15 +0100

    finished 5.3.4 updated main

--- src/cs107/Main.java
+++ src/cs107/Main.java
@@ -58,7 +58,7 @@ public final class Main {
         assert testQoiOpRGB();
         assert testQoiOpRGBA();
         assert testQoiOpIndex();
-        //assert testQoiOpDiff();
+        assert testQoiOpDiff();
         //assert testQoiOpLuma();
         //assert testQoiOpRun();
         //assert testEncodeData();
