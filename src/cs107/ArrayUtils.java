72c3b9c17d3f224b738fc6af32e1e158d6a5fa5a
rperellomas <ricardo.perellomas@epfl.ch>
2022-11-12 18:50:19 +0100

    array utils fixed toInt

--- src/cs107/ArrayUtils.java
+++ src/cs107/ArrayUtils.java
@@ -1,13 +1,10 @@
 package cs107;
 
-import java.util.Arrays;
-
 /**
  * Utility class to manipulate arrays.
- *
+ * @apiNote First Task of the 2022 Mini Project
  * @author Hamza REMMAL (hamza.remmal@epfl.ch)
  * @version 1.3
- * @apiNote First Task of the 2022 Mini Project
  * @since 1.0
  */
 public final class ArrayUtils {
@@ -15,8 +12,7 @@ public final class ArrayUtils {
     /**
      * DO NOT CHANGE THIS, MORE ON THAT IN WEEK 7.
      */
-    private ArrayUtils() {
-    }
+    private ArrayUtils(){}
 
     // ==================================================================================
     // =========================== ARRAY EQUALITY METHODS ===============================
@@ -24,7 +20,6 @@ public final class ArrayUtils {
 
     /**
      * Check if the content of both arrays is the same
-     *
      * @param a1 (byte[]) - First array
      * @param a2 (byte[]) - Second array
      * @return (boolean) - true if both arrays have the same content (or both null), false otherwise
@@ -51,9 +46,10 @@ public final class ArrayUtils {
         return true; //if all elements are the same then return true
     }
 
+
+
     /**
      * Check if the content of both arrays is the same
-     *
      * @param a1 (byte[][]) - First array
      * @param a2 (byte[][]) - Second array
      * @return (boolean) - true if both arrays have the same content (or both null), false otherwise
@@ -98,7 +94,6 @@ public final class ArrayUtils {
 
     /**
      * Wrap the given value in an array
-     *
      * @param value (byte) - value to wrap
      * @return (byte[]) - array with one element (value)
      */
@@ -115,7 +110,6 @@ public final class ArrayUtils {
      * Create an Integer using the given array. The input needs to be considered
      * as "Big Endian"
      * (See handout for the definition of "Big Endian")
-     *
      * @param bytes (byte[]) - Array of 4 bytes
      * @return (int) - Integer representation of the array
      * @throws AssertionError if the input is null or the input's length is different from 4
@@ -126,7 +120,7 @@ public final class ArrayUtils {
         int element; //temporary variable that holds element i
         int value = 0; //variable that holds the concatenated values
         for (int i = 0; i < 4; i++) {
-            element = bytes[i];
+            element = bytes[i] & 0b11111111;
             element = element << 32 - (8 * (i + 1)); //shift left to the leftmost 8-bits in a 32-bit number. then shifts left to the second leftmost 8-bits.... doesn't shift
             value = value | element; //does a 'bitwise or' to put together the current 'value' with the new 'element'
         }
@@ -136,7 +130,6 @@ public final class ArrayUtils {
     /**
      * Separate the Integer (word) to 4 bytes. The Memory layout of this integer is "Big Endian"
      * (See handout for the definition of "Big Endian")
-     *
      * @param value (int) - The integer
      * @return (byte[]) - Big Endian representation of the integer
      */
@@ -157,7 +150,6 @@ public final class ArrayUtils {
 
     /**
      * Concatenate a given sequence of bytes and stores them in an array
-     *
      * @param bytes (byte ...) - Sequence of bytes to store in the array
      * @return (byte[]) - Array representation of the sequence
      * @throws AssertionError if the input is null
@@ -172,11 +164,10 @@ public final class ArrayUtils {
 
     /**
      * Concatenate a given sequence of arrays into one array
-     *
      * @param tabs (byte[] ...) - Sequence of arrays
      * @return (byte[]) - Array representation of the sequence
      * @throws AssertionError if the input is null
-     *                        or one of the inner arrays of input is null.
+     * or one of the inner arrays of input is null.
      */
     public static byte[] concat(byte[]... tabs) {
         assert (!(tabs == null)); //checks if arrays within arrays is null
@@ -205,20 +196,18 @@ public final class ArrayUtils {
         return concatarray;
     }
 
-
     // ==================================================================================
     // =========================== ARRAY EXTRACTION METHODS =============================
     // ==================================================================================
 
     /**
      * Extract an array from another array
-     *
-     * @param input  (byte[]) - Array to extract from
-     * @param start  (int) - Index in the input array to start the extract from
+     * @param input (byte[]) - Array to extract from
+     * @param start (int) - Index in the input array to start the extract from
      * @param length (int) - The number of bytes to extract
      * @return (byte[]) - The extracted array
      * @throws AssertionError if the input is null or start and length are invalid.
-     *                        start + length should also be smaller than the input's length
+     * start + length should also be smaller than the input's length
      */
     public static byte[] extract(byte[] input, int start, int length) {
         assert input != null; //break if input is null
@@ -240,13 +229,12 @@ public final class ArrayUtils {
     /**
      * Create a partition of the input array.
      * (See handout for more information on how this method works)
-     *
      * @param input (byte[]) - The original array
      * @param sizes (int ...) - Sizes of the partitions
      * @return (byte[][]) - Array of input's partitions.
      * The order of the partition is the same as the order in sizes
      * @throws AssertionError if one of the parameters is null
-     *                        or the sum of the elements in sizes is different from the input's length
+     * or the sum of the elements in sizes is different from the input's length
      */
     public static byte[][] partition(byte[] input, int... sizes) {
         assert input != null; //check if input is not null
@@ -285,7 +273,6 @@ public final class ArrayUtils {
         return partitions;
     }
 
-
     // ==================================================================================
     // ============================== ARRAY FORMATTING METHODS ==========================
     // ==================================================================================
@@ -296,11 +283,10 @@ public final class ArrayUtils {
      * a 2-dim byte array where the first dimension is the pixel
      * and the second dimension is the channel.
      * See handouts for more information on the format.
-     *
      * @param input (int[][]) - image data
      * @return (byte [][]) - formatted image data
      * @throws AssertionError if the input is null
-     *                        or one of the inner arrays of input is null
+     * or one of the inner arrays of input is null
      */
     public static byte[][] imageToChannels(int[][] input) {
         assert (input.length != 0); //checks that input is not null
@@ -330,16 +316,15 @@ public final class ArrayUtils {
      * Format a 2-dim byte array where the first dimension is the pixel
      * and the second is the channel to a 2-dim int array where the first
      * dimension is the height and the second is the width
-     *
-     * @param input  (byte[][]) : linear representation of the image
+     * @param input (byte[][]) : linear representation of the image
      * @param height (int) - Height of the resulting image
-     * @param width  (int) - Width of the resulting image
+     * @param width (int) - Width of the resulting image
      * @return (int[][]) - the image data
      * @throws AssertionError if the input is null
-     *                        or one of the inner arrays of input is null
-     *                        or input's length differs from width * height
-     *                        or height is invalid
-     *                        or width is invalid
+     * or one of the inner arrays of input is null
+     * or input's length differs from width * height
+     * or height is invalid
+     * or width is invalid
      */
     public static int[][] channelsToImage(byte[][] input, int height, int width) {
         assert input.length != 0; //assert input and input[i] is not null
